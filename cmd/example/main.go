b9e7df09df519db938a49191624d7c14a6ed04ab
Michael Donahue <81647737+mdonahue-godaddy@users.noreply.github.com>
2024-02-02 17:55:07 -0600

    Add client side cert

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,9 +1,132 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"time"
+
 	"github.com/mjdusa/go-self-signed-cert-http-server/pkg/cert"
 )
 
 func main() {
-	cert.Example()
+	Example()
+}
+
+// based loosly on https://shaneutt.com/blog/golang-ca-and-signed-cert-go/
+// WARNING: This is not a production ready example. It is for educational purposes only.
+// WARNING / NOTICE: Some of thes steps could be optimized by changing the order of operations, DON'T do it, it changes the behavior of the code and will break the cert creation process.
+
+func Example() {
+	now := time.Now().UTC()
+	notBefore := now
+	notAfter := now.AddDate(1, 0, 0) // 1 year from now
+	caSerNbr := cert.CreateSerialNumber(1965)
+	serverSerNbr := cert.CreateSerialNumber(now.Year() - 1)
+
+	subject, err := cert.CreateSubject([]string{"Go Example Company, Inc."}, []string{"US"}, []string{"IA"}, []string{"Cedar Rapids"}, []string{"123 Main Street"}, []string{"52401"})
+	if err != nil {
+		panic(err)
+	}
+
+	// create CA cert
+	//caCert, caCertBytes, caPEM, caPrivKey, caPrivKeyPEM, err := cert.CreateSelfSignedCA(4096, caSerNbr, subject, notBefore, notBefore.AddDate(10, 0, 0))
+	caCert, _, _, caPrivKey, _, err := cert.CreateSelfSignedCA(4096, caSerNbr, subject, notBefore, notBefore.AddDate(10, 0, 0))
+	if err != nil {
+		panic(err)
+	}
+
+	// create server cert
+	//cert, certBytes, certPEM, privKey, privKeyPEM, err := cert.CreateSelfSignedCertificate(caCert, caPrivKey, 4096, serverSerNbr, subject, notBefore, notAfter)
+	_, _, serverPEM, _, serverPrivKeyPEM, err := cert.CreateSelfSignedCertificate(caCert, caPrivKey, 4096, serverSerNbr, subject, notBefore, notAfter)
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert to TLS cert
+	serverCert, err := tls.X509KeyPair(serverPEM.Bytes(), serverPrivKeyPEM.Bytes())
+	if err != nil {
+		panic(err)
+	}
+
+	clientSerNbr := cert.CreateSerialNumber(now.Year())
+
+	// create client cert
+	//cert, certBytes, certPEM, privKey, privKeyPEM, err := cert.CreateSelfSignedCertificate(caCert, caPrivKey, 4096, clientSerNbr, subject, notBefore, notAfter)
+	_, _, clientPEM, _, clientPrivKeyPEM, err := cert.CreateSelfSignedCertificate(caCert, caPrivKey, 4096, clientSerNbr, subject, notBefore, notAfter)
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert to TLS cert
+	clientCert, err := tls.X509KeyPair(clientPEM.Bytes(), clientPrivKeyPEM.Bytes())
+	if err != nil {
+		panic(err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AddCert(caCert)
+	// alternatively, you can use AppendCertsFromPEM, but it is not as efficient as AddCert
+	//caCertPool.AppendCertsFromPEM(caPEM.Bytes())
+
+	// setup server & start
+	serverTLSConf := &tls.Config{
+		RootCAs:      caCertPool.Clone(),
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    caCertPool.Clone(),
+		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			// called after normal certificate verification, client cert is in verified first
+			// do custom verification here
+			return nil
+		},
+	}
+
+	server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "success!")
+	}))
+
+	server.TLS = serverTLSConf
+	server.StartTLS()
+	defer server.Close()
+
+	// setup client & make request
+	clientTLSConf := &tls.Config{
+		RootCAs:      caCertPool.Clone(),
+		Certificates: []tls.Certificate{clientCert},
+		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			// called after normal certificate verification, client cert is in verified first
+			// do custom verification here
+			return nil
+		},
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: clientTLSConf,
+	}
+
+	client := http.Client{
+		Transport: transport,
+	}
+
+	resp, err := client.Get(server.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	respBodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	body := strings.TrimSpace(string(respBodyBytes[:]))
+	if body == "success!" {
+		fmt.Println(body)
+	} else {
+		panic("not successful!")
+	}
 }
--- pkg/cert/cert.go
+++ pkg/cert/cert.go
@@ -4,17 +4,11 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"fmt"
-	"io"
 	"math/big"
 	"net"
-	"net/http"
-	"net/http/httptest"
-	"strings"
 	"time"
 )
 
@@ -23,82 +17,8 @@ const (
 	RSAPrivateKey = "RSA PRIVATE KEY"
 )
 
-// based loosly on https://shaneutt.com/blog/golang-ca-and-signed-cert-go/
-// WARNING: This is not a production ready example. It is for educational purposes only.
-// WARNING / NOTICE: Some of thes steps could be optimized by changing the order of operations, DON'T do it, it changes the behavior of the code and will break the cert creation process.
-
-func Example() {
-	now := time.Now().UTC()
-	caSerNbr := CreateSerialNumber(1965)
-	certSerNbr := CreateSerialNumber(now.Year())
-
-	subject, err := CreateSubject([]string{"Go Example Company, Inc."}, []string{"US"}, []string{"IA"}, []string{"Cedar Rapids"}, []string{"123 Main Street"}, []string{"52401"})
-	if err != nil {
-		panic(err)
-	}
-
-	//caCert, caPEM, caPrivKey, caPrivKeyPEM, err := CreateCA(caSerNbr, subject, now, now.AddDate(10, 0, 0))
-	caCert, caPEM, caPrivKey, _, err := CreateCA(caSerNbr, subject, now, now.AddDate(10, 0, 0))
-	if err != nil {
-		panic(err)
-	}
-
-	notAfter := now.AddDate(1, 0, 0) // 1 year from now
-	//certificate, certPEM, certPrivKey, certPrivKeyPEM, err := CreateCertificate(caCert, caPrivKey, certSerNbr, subject, now, notAfter)
-	_, certPEM, _, certPrivKeyPEM, err := CreateCertificate(caCert, caPrivKey, certSerNbr, subject, now, notAfter)
-	if err != nil {
-		panic(err)
-	}
-
-	serverCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivKeyPEM.Bytes())
-	if err != nil {
-		panic(err)
-	}
-
-	serverTLSConf := &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-	}
-
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caPEM.Bytes())
-
-	clientTLSConf := &tls.Config{
-		RootCAs: caCertPool,
-	}
-
-	server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "success!")
-	}))
-
-	server.TLS = serverTLSConf
-	server.StartTLS()
-	defer server.Close()
-
-	transport := &http.Transport{
-		TLSClientConfig: clientTLSConf,
-	}
-
-	http := http.Client{
-		Transport: transport,
-	}
-
-	resp, err := http.Get(server.URL)
-	if err != nil {
-		panic(err)
-	}
-
-	respBodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	body := strings.TrimSpace(string(respBodyBytes[:]))
-	if body == "success!" {
-		fmt.Println(body)
-	} else {
-		panic("not successful!")
-	}
-}
+// WARNING / NOTICE: Some of thes steps could be optimized by changing the order of operations.
+//                   DON'T change the order, it will change the behavior of the code and can break the cert creation process.
 
 func CreateSerialNumber(nbr int) *big.Int {
 	return big.NewInt(int64(nbr))
@@ -137,9 +57,51 @@ func PEMEncodeBlock(blockType string, byArray []byte) (*bytes.Buffer, error) {
 	return buffer, nil
 }
 
-func CreateCA(serialNumber *big.Int, subject pkix.Name, notBefore, notAfter time.Time) (*x509.Certificate, *bytes.Buffer, *rsa.PrivateKey, *bytes.Buffer, error) {
-	// define CA certificate
-	caTemplate := &x509.Certificate{
+func createCertWithRSA(privateKeyBits int, template *x509.Certificate, parent *x509.Certificate, caPrivKey *rsa.PrivateKey) (*x509.Certificate, []byte, *bytes.Buffer, *rsa.PrivateKey, *bytes.Buffer, error) {
+	fail := func(err error) (*x509.Certificate, []byte, *bytes.Buffer, *rsa.PrivateKey, *bytes.Buffer, error) {
+		return nil, []byte{}, nil, nil, nil, err
+	}
+
+	// gen private key
+	privKey, pkErr := CreatePrivateKey(privateKeyBits)
+	if pkErr != nil {
+		return fail(pkErr)
+	}
+
+	if caPrivKey == nil {
+		caPrivKey = privKey
+	}
+
+	// create certificate
+	certBytes, cErr := x509.CreateCertificate(rand.Reader, template, parent, &privKey.PublicKey, caPrivKey)
+	if cErr != nil {
+		return fail(cErr)
+	}
+
+	// PEM encode certificate
+	certPEM, ecErr := PEMEncodeBlock(Certificate, certBytes)
+	if ecErr != nil {
+		return fail(ecErr)
+	}
+
+	// PEM encode private key
+	privKeyPEM, peErr := PEMEncodeBlock(RSAPrivateKey, x509.MarshalPKCS1PrivateKey(privKey))
+	if peErr != nil {
+		return fail(peErr)
+	}
+
+	// parse certificate bytes to x509.Certificate
+	cert, pcErr := x509.ParseCertificate(certBytes)
+	if pcErr != nil {
+		return fail(pcErr)
+	}
+
+	return cert, certBytes, certPEM, privKey, privKeyPEM, nil
+}
+
+func CreateSelfSignedCA(privateKeyBits int, serialNumber *big.Int, subject pkix.Name, notBefore, notAfter time.Time) (*x509.Certificate, []byte, *bytes.Buffer, *rsa.PrivateKey, *bytes.Buffer, error) {
+	// define CA certificate template
+	template := &x509.Certificate{
 		SerialNumber:          serialNumber,
 		Subject:               subject,
 		NotBefore:             notBefore,
@@ -150,42 +112,12 @@ func CreateCA(serialNumber *big.Int, subject pkix.Name, notBefore, notAfter time
 		BasicConstraintsValid: true,
 	}
 
-	// gen private key
-	privKey, pkErr := CreatePrivateKey(4096)
-	if pkErr != nil {
-		return nil, nil, nil, nil, pkErr
-	}
-
-	// create CA certificate
-	certBytes, cErr := x509.CreateCertificate(rand.Reader, caTemplate, caTemplate, &privKey.PublicKey, privKey)
-	if cErr != nil {
-		return nil, nil, nil, nil, cErr
-	}
-
-	// PEM encode CA certificate
-	pem, ecErr := PEMEncodeBlock(Certificate, certBytes)
-	if ecErr != nil {
-		return nil, nil, nil, nil, ecErr
-	}
-
-	// PEM encode private key
-	privKeyPEM, pErr := PEMEncodeBlock(RSAPrivateKey, x509.MarshalPKCS1PrivateKey(privKey))
-	if pErr != nil {
-		return nil, nil, nil, nil, pErr
-	}
-
-	// parse CA certificate bytes to x509.Certificate
-	caCert, pcErr := x509.ParseCertificate(certBytes)
-	if pcErr != nil {
-		return nil, nil, nil, nil, pcErr
-	}
-
-	return caCert, pem, privKey, privKeyPEM, nil
+	return createCertWithRSA(privateKeyBits, template, template, nil)
 }
 
-func CreateCertificate(caCert *x509.Certificate, caPrivKey *rsa.PrivateKey, serialNumber *big.Int, subject pkix.Name, notBefore, notAfter time.Time) (*x509.Certificate, *bytes.Buffer, *rsa.PrivateKey, *bytes.Buffer, error) {
-	// define certificate
-	certTemplate := &x509.Certificate{
+func CreateSelfSignedCertificate(caCert *x509.Certificate, caPrivKey *rsa.PrivateKey, privateKeyBits int, serialNumber *big.Int, subject pkix.Name, notBefore, notAfter time.Time) (*x509.Certificate, []byte, *bytes.Buffer, *rsa.PrivateKey, *bytes.Buffer, error) {
+	// define certificate template
+	template := &x509.Certificate{
 		SerialNumber: serialNumber,
 		Subject:      subject,
 		NotBefore:    notBefore,
@@ -197,35 +129,5 @@ func CreateCertificate(caCert *x509.Certificate, caPrivKey *rsa.PrivateKey, seri
 		KeyUsage:     x509.KeyUsageDigitalSignature,
 	}
 
-	// gen private key
-	privKey, pkErr := CreatePrivateKey(4096)
-	if pkErr != nil {
-		return nil, nil, nil, nil, pkErr
-	}
-
-	// create certificate
-	certBytes, ccErr := x509.CreateCertificate(rand.Reader, certTemplate, caCert, &privKey.PublicKey, caPrivKey)
-	if ccErr != nil {
-		return nil, nil, nil, nil, ccErr
-	}
-
-	// PEM encode certificate
-	pem, ceErr := PEMEncodeBlock(Certificate, certBytes)
-	if ceErr != nil {
-		return nil, nil, nil, nil, ceErr
-	}
-
-	// PEM encode private key
-	privKeyPEM, peErr := PEMEncodeBlock(RSAPrivateKey, x509.MarshalPKCS1PrivateKey(privKey))
-	if peErr != nil {
-		return nil, nil, nil, nil, peErr
-	}
-
-	// parse certificate bytes to x509.Certificate
-	certificate, pcErr := x509.ParseCertificate(certBytes)
-	if pcErr != nil {
-		return nil, nil, nil, nil, pcErr
-	}
-
-	return certificate, pem, privKey, privKeyPEM, nil
+	return createCertWithRSA(privateKeyBits, template, caCert, caPrivKey)
 }
